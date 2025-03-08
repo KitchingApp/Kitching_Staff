cded6764f4575b1cb9233b89d1389e349b3447c9
kimbangto <kimchaeyeon.525@gmail.com>
2025-03-08 21:52:13 +0900

    fix: 불필요한 토큰 호출 수정

--- feature/main/src/main/java/com/kitching/main/navigation/MainNavGraph.kt
+++ feature/main/src/main/java/com/kitching/main/navigation/MainNavGraph.kt
@@ -6,12 +6,10 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.kitching.core.common.ScreenRouteDef
 import com.kitching.main.EntryPointScreen
-import com.kitching.main.fcm.getFcmToken
 
 fun NavGraphBuilder.mainNavGraph(
     navController: NavHostController
 ) {
-    getFcmToken()
     navigation(
         route = ScreenRouteDef.MainGraph.routeName,
         startDestination = ScreenRouteDef.Entry.routeName,
