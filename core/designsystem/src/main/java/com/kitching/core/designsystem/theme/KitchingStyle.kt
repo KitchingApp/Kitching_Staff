2fa7041f8857f8402ce7f65c2baef69fd46ec44e
Eddie-Pak <foalstn@naver.com>
2025-06-25 11:42:55 +0900

    design: KitchingDimens 생성

---
+++ core/designsystem/src/main/java/com/kitching/core/designsystem/theme/KitchingStyle.kt
@@ -0,0 +1,45 @@
+package com.kitching.core.designsystem.theme
+
+import androidx.compose.ui.unit.dp
+
+object KitchingDimens {
+    object Corner {
+        val xxxSmall = 2.dp
+        val xxSmall = 4.dp
+        val xSmall = 8.dp
+        val small = 12.dp
+        val medium = 16.dp
+        val large = 24.dp
+        val xLarge = 32.dp
+        val xxLarge = 40.dp
+    }
+
+    object Margin {
+        val xxxSmall = 2.dp
+        val xxSmall = 4.dp
+        val xSmall = 8.dp
+        val small = 12.dp
+        val medium = 16.dp
+        val large = 24.dp
+        val xLarge = 32.dp
+        val xxLarge = 40.dp
+
+        object Layout {
+            val horizontal = small
+            val vertical = xSmall
+        }
+    }
+
+    object Spacing {
+        val xxxSmall = 2.dp
+        val xxSmall = 4.dp
+        val xSmall = 8.dp
+        val small = 12.dp
+        val medium = 16.dp
+        val large = 24.dp
+        val xLarge = 32.dp
+        val xxLarge = 40.dp
+        val xxxLarge = 52.dp
+        val x4Large = 64.dp
+    }
+}
