342ec62d98df29857c33a9b5a166da631ab15244
Eddie-Pak <foalstn@naver.com>
2025-07-02 14:17:31 +0900

    refactor: TodoPrep entities, DTO 생성

--- domain/src/main/java/com/kitching/domain/entities/TodoPrep.kt
+++ domain/src/main/java/com/kitching/domain/entities/TodoPrep.kt
@@ -1,11 +1,11 @@
 package com.kitching.domain.entities
 
 data class TodoPrep(
-    val id: String = "",
-    val date: String = "",
-    val categoryId: String = "",
-    val prepId: String = "",
-    val isDone: Boolean = false,
+    val id: String,
+    val date: String,
+    val categoryId: String,
+    val prepId: String,
+    val isDone: Boolean,
 )
 
 data class TodoPrepWithDetails(
