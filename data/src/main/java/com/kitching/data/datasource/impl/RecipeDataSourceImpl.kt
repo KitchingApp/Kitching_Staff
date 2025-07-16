4849be35c700e2effc62422ffb8458c876d2a468
Eddie-Pak <foalstn@naver.com>
2025-07-17 02:02:06 +0900

    feat: RecipeDataSource Exception 적용

--- data/src/main/java/com/kitching/data/datasource/impl/RecipeDataSourceImpl.kt
+++ data/src/main/java/com/kitching/data/datasource/impl/RecipeDataSourceImpl.kt
@@ -7,31 +7,28 @@ import com.kitching.data.dto.RecipeDTO
 import com.kitching.data.firebase.COLLECTION_INGREDIENT
 import com.kitching.data.firebase.COLLECTION_RECIPE
 import com.kitching.data.firebase.DOCUMENT_TEAM_ID
+import com.kitching.data.util.ExceptionHandler
 import kotlinx.coroutines.tasks.await
 
 class RecipeDataSourceImpl(
     private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
 ) : RecipeDataSource {
-    override suspend fun getRecipes(teamId: String): List<RecipeDTO> {
-        return try {
-            db.collection(COLLECTION_RECIPE)
-                .whereEqualTo(DOCUMENT_TEAM_ID, teamId)
-                .get()
-                .await()
-                .documents
-                .mapNotNull { documentSnapshot ->
-                    val recipeDTO = documentSnapshot.toObject(RecipeDTO::class.java)
+    override suspend fun getRecipes(teamId: String): List<RecipeDTO> = ExceptionHandler.safeCall {
+        db.collection(COLLECTION_RECIPE)
+            .whereEqualTo(DOCUMENT_TEAM_ID, teamId)
+            .get()
+            .await()
+            .documents
+            .mapNotNull { documentSnapshot ->
+                val recipeDTO = documentSnapshot.toObject(RecipeDTO::class.java)
 
-                    val ingredients = documentSnapshot.reference
-                        .collection(COLLECTION_INGREDIENT)
-                        .get()
-                        .await()
-                        .toObjects(IngredientDTO::class.java)
+                val ingredients = documentSnapshot.reference
+                    .collection(COLLECTION_INGREDIENT)
+                    .get()
+                    .await()
+                    .toObjects(IngredientDTO::class.java)
 
-                    recipeDTO?.copy(ingredient = ingredients)
-                }
-        } catch (e: Exception) {
-            emptyList()
-        }
+                recipeDTO?.copy(ingredient = ingredients)
+            }
     }
 }
