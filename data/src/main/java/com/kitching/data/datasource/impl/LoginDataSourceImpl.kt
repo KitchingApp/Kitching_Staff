6329da84f70abcd8579c091dc5866348e2b1d562
Eddie-Pak <foalstn@naver.com>
2025-07-16 20:45:56 +0900

    feat: Login Exception 적용

--- data/src/main/java/com/kitching/data/datasource/impl/LoginDataSourceImpl.kt
+++ data/src/main/java/com/kitching/data/datasource/impl/LoginDataSourceImpl.kt
@@ -6,25 +6,44 @@ import com.kitching.data.dto.UserDTO
 import com.kitching.data.firebase.COLLECTION_USER
 import com.kitching.data.firebase.FIELD_USER_IMAGE
 import com.kitching.data.firebase.FIELD_USER_NAME
+import com.kitching.data.util.ExceptionHandler
+import com.kitching.data.util.KitchingRuntimeException
 import kotlinx.coroutines.tasks.await
 
 class LoginDataSourceImpl(
-    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-): LoginDataSource {
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance(),
+) : LoginDataSource {
     override suspend fun checkAndSaveUser(
         userId: String,
         userName: String,
         userImage: String,
-    ): Boolean = runCatching {
-        val userDoc = db.collection(COLLECTION_USER).document(userId).get().await().toObject(UserDTO::class.java)
-        if(userDoc == null) {
-            db.collection(COLLECTION_USER).add(UserDTO(id = userId, userName = userName, userImage = userImage)).await()
-        } else {
-            db.collection(COLLECTION_USER).document(userId).update(FIELD_USER_NAME, userName, FIELD_USER_IMAGE, userImage).await()
-        }
-    }.isSuccess
+    ): Boolean = ExceptionHandler.safeCall {
+        val userDoc = db.collection(COLLECTION_USER).document(userId).get().await()
+            .toObject(UserDTO::class.java)
 
-    override suspend fun getUserById(userId: String): UserDTO {
-        return db.collection(COLLECTION_USER).document(userId).get().await().toObject(UserDTO::class.java) ?: UserDTO()
+        if (userDoc == null) {
+            db.collection(COLLECTION_USER)
+                .add(UserDTO(id = userId, userName = userName, userImage = userImage)).await()
+
+            val savedUser = db.collection(COLLECTION_USER).document(userId).get().await()
+                .toObject(UserDTO::class.java)
+
+            if (savedUser == null) {
+                throw KitchingRuntimeException.UserSaveFailedException()
+            }
+
+            true
+        } else {
+            db.collection(COLLECTION_USER).document(userId)
+                .update(FIELD_USER_NAME, userName, FIELD_USER_IMAGE, userImage).await()
+
+            true
+        }
+    }
+
+    override suspend fun getUserById(userId: String): UserDTO = ExceptionHandler.safeCall {
+        val userDoc = db.collection(COLLECTION_USER).document(userId).get().await()
+            .toObject(UserDTO::class.java)
+        userDoc ?: throw KitchingRuntimeException.UserNotFoundException()
     }
 }
