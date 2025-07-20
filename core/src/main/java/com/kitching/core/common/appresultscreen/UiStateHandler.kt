3d7a3ecc5f711b0280bf8fd149a6da0843f13d15
Eddie-Pak <foalstn@naver.com>
2025-07-20 19:33:08 +0900

    feat: CombinedUiStateHandler 구현

--- core/src/main/java/com/kitching/core/common/appresultscreen/UiStateHandler.kt
+++ core/src/main/java/com/kitching/core/common/appresultscreen/UiStateHandler.kt
@@ -3,6 +3,13 @@ package com.kitching.core.common.appresultscreen
 import androidx.compose.runtime.Composable
 import com.kitching.domain.util.UiState
 
+/**
+ * UiState Handler - UiState를 화면에서 처리
+ *
+ * @param uiState 처리할 상태 값
+ * @param onRetry 에러 상태에서 재시도 함수
+ * @param onSuccess 성공일 때 보여줄 화면
+ */
 @Composable
 fun <T> UiStateHandler(
     uiState: UiState<T>,
@@ -22,6 +29,48 @@ fun <T> UiStateHandler(
 
         uiState.isError -> {
             ErrorScreen(uiState.error ?: "") { onRetry() }
+        }
+    }
+}
+
+/**
+ * Combined UiState Handler - 두 개의 UiState를 동시에 처리
+ *
+ * @param firstState 첫 번째 상태
+ * @param secondState 두 번째 상태
+ * @param onRetry 에러 상태에서 재시도 함수
+ * @param onSuccess 두 상태 모두 성공일 때 보여줄 화면
+ */
+@Composable
+fun <T, R> CombinedUiStateHandler(
+    firstState: UiState<T>,
+    secondState: UiState<R>,
+    onRetry: Pair<() -> Unit, () -> Unit> = Pair({}, {}),
+    onSuccess: @Composable (T, R) -> Unit
+) {
+    when {
+        firstState.isLoading || secondState.isLoading -> {
+            ProgressIndicatorScreen()
+        }
+
+        firstState.isError || secondState.isError -> {
+            val errorMessage = firstState.error ?: secondState.error ?: "알 수 없는 오류가 발생했습니다."
+            ErrorScreen(errorMessage) {
+                if (firstState.isError) {
+                    onRetry.first()
+                }
+                if (secondState.isError) {
+                    onRetry.second()
+                }
+            }
+        }
+
+        firstState.isSuccess && secondState.isSuccess -> {
+            firstState.data?.let { firstData ->
+                secondState.data?.let { secondData ->
+                    onSuccess(firstData, secondData)
+                }
+            }
         }
     }
 }
