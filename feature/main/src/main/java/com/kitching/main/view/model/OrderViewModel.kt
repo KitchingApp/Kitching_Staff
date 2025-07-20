f0d98da06d67a2273e57a50e1c9cbb657ff7cf3b
Eddie-Pak <foalstn@naver.com>
2025-07-20 19:33:38 +0900

    refactor: OrderTabScreen, OrderViewModel UiState 적용

--- feature/main/src/main/java/com/kitching/main/view/model/OrderViewModel.kt
+++ feature/main/src/main/java/com/kitching/main/view/model/OrderViewModel.kt
@@ -6,6 +6,7 @@ import com.kitching.domain.entities.Order
 import com.kitching.domain.entities.OrderCategory
 import com.kitching.domain.repository.OrderRepository
 import com.kitching.domain.util.AppResult
+import com.kitching.domain.util.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
@@ -13,27 +14,59 @@ import kotlinx.coroutines.launch
 
 class OrderViewModel(private val orderRepository: OrderRepository) : ViewModel() {
 
-    private val _orderCategories = MutableStateFlow<AppResult<List<OrderCategory>>>(AppResult.Initial)
+    private val _orderCategories = MutableStateFlow(UiState<List<OrderCategory>>())
     val orderCategories get() = _orderCategories.asStateFlow()
 
     fun fetchOrderCategories(teamId: String) {
         viewModelScope.launch {
-            orderRepository.getOrderCategories(teamId)
-                .collectLatest {
-                    _orderCategories.value = it
+            orderRepository.getOrderCategories(teamId).collectLatest { result ->
+                when (result) {
+                    is AppResult.Initial -> {
+                        _orderCategories.value = _orderCategories.value
+                    }
+
+                    is AppResult.Loading -> {
+                        _orderCategories.value = _orderCategories.value.toLoading()
+                    }
+
+                    is AppResult.Success -> {
+                        _orderCategories.value = _orderCategories.value.toSuccess(result.data)
+                    }
+
+                    is AppResult.Failure -> {
+                        _orderCategories.value =
+                            _orderCategories.value.toError(result.exception.message ?: "")
+                    }
                 }
+            }
         }
     }
 
-    private val _orderItems = MutableStateFlow<AppResult<List<Order>>>(AppResult.Initial)
+    private val _orderItems = MutableStateFlow(UiState<List<Order>>())
     val orderItems get() = _orderItems.asStateFlow()
 
     fun fetchOrderItems(teamId: String) {
         viewModelScope.launch {
-            orderRepository.getOrderItems(teamId)
-                .collectLatest {
-                    _orderItems.value = it
+            orderRepository.getOrderItems(teamId).collectLatest { result ->
+                when (result) {
+                    is AppResult.Initial -> {
+                        _orderItems.value = _orderItems.value
+                    }
+
+                    is AppResult.Loading -> {
+                        _orderItems.value = _orderItems.value.toLoading()
+                    }
+
+                    is AppResult.Success -> {
+                        _orderItems.value = _orderItems.value.toSuccess(result.data)
+                    }
+
+                    is AppResult.Failure -> {
+                        _orderItems.value =
+                            _orderItems.value.toError(result.exception.message ?: "")
+                    }
                 }
+            }
         }
     }
 }
