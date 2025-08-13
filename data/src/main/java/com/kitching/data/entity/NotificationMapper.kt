b694dc0a4e5b5f6105cd9debf1e7beb8c9fed9e3
Eddie-Pak <foalstn@naver.com>
2025-08-13 17:19:41 +0900

    feat: Notification Mapper 생성

---
+++ data/src/main/java/com/kitching/data/entity/NotificationMapper.kt
@@ -0,0 +1,34 @@
+package com.kitching.data.entity
+
+import com.kitching.domain.entities.NoticeNotification
+import com.kitching.domain.entities.ScheduleNotification
+
+fun ScheduleNotification.toEntity(): ScheduleNotificationEntity = ScheduleNotificationEntity(
+    id = this.id,
+    scheduleDate = this.scheduleDate,
+    scheduleTimeName = this.scheduleTimeName,
+    rejectReason = this.rejectReason
+)
+
+fun ScheduleNotificationEntity.toDomain(): ScheduleNotification = ScheduleNotification(
+    id = this.id,
+    scheduleDate = this.scheduleDate,
+    scheduleTimeName = this.scheduleTimeName,
+    rejectReason = this.rejectReason
+)
+
+fun List<ScheduleNotificationEntity>.toDomainList(): List<ScheduleNotification> = this.map { it.toDomain() }
+
+fun NoticeNotification.toEntity(): NoticeNotificationEntity = NoticeNotificationEntity(
+    id = this.id,
+    title = this.title,
+    writerName = this.writerName
+)
+
+fun NoticeNotificationEntity.toDomain(): NoticeNotification = NoticeNotification(
+    id = this.id,
+    title = this.title,
+    writerName = this.writerName
+)
+
+fun List<NoticeNotificationEntity>.toDomainList(): List<NoticeNotification> = this.map { it.toDomain() }
