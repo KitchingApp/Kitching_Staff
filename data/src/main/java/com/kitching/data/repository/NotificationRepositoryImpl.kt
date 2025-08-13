3ce62d3f562588c80cec391754c22e7b823a6bdf
Eddie-Pak <foalstn@naver.com>
2025-08-13 17:20:32 +0900

    feat: Notification Repository 구현

---
+++ data/src/main/java/com/kitching/data/repository/NotificationRepositoryImpl.kt
@@ -0,0 +1,95 @@
+package com.kitching.data.repository
+
+import com.kitching.data.dao.NotificationDAO
+import com.kitching.data.entity.toDomainList
+import com.kitching.data.entity.toEntity
+import com.kitching.domain.entities.NoticeNotification
+import com.kitching.domain.entities.ScheduleNotification
+import com.kitching.domain.repository.NotificationRepository
+import com.kitching.domain.util.AppResult
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+
+class NotificationRepositoryImpl(
+    private val notificationDAO: NotificationDAO,
+) : NotificationRepository {
+    override suspend fun insertScheduleNotification(notification: ScheduleNotification) = flow {
+        emit(AppResult.Loading)
+
+        emit(AppResult.Success(
+            notificationDAO.insertScheduleNotification(notification.toEntity())
+        ))
+    }.catch {
+        emit(AppResult.Failure(it))
+    }
+
+    override suspend fun getScheduleNotifications() = flow {
+        emit(AppResult.Loading)
+
+        emit(AppResult.Success(
+            notificationDAO.getScheduleNotifications().toDomainList()
+        ))
+    }.catch {
+        emit(AppResult.Failure(it))
+    }
+
+    override suspend fun deleteScheduleNotification(id: Long) = flow {
+        emit(AppResult.Loading)
+
+        emit(AppResult.Success(
+            notificationDAO.deleteScheduleNotification(id)
+        ))
+    }.catch {
+        emit(AppResult.Failure(it))
+    }
+
+    override suspend fun deleteAllScheduleNotification() = flow {
+        emit(AppResult.Loading)
+
+        emit(AppResult.Success(
+            notificationDAO.deleteAllScheduleNotification()
+        ))
+    }.catch {
+        emit(AppResult.Failure(it))
+    }
+
+    override suspend fun insertNoticeNotification(notification: NoticeNotification) =flow {
+        emit(AppResult.Loading)
+
+        emit(AppResult.Success(
+            notificationDAO.insertNoticeNotification(notification.toEntity())
+        ))
+    }.catch {
+        emit(AppResult.Failure(it))
+    }
+
+    override suspend fun getNoticeNotifications() = flow {
+        emit(AppResult.Loading)
+
+        emit(AppResult.Success(
+            notificationDAO.getNoticeNotifications().toDomainList()
+        ))
+    }.catch {
+        emit(AppResult.Failure(it))
+    }
+
+    override suspend fun deleteNoticeNotification(id: Long) = flow {
+        emit(AppResult.Loading)
+
+        emit(AppResult.Success(
+            notificationDAO.deleteNoticeNotification(id)
+        ))
+    }.catch {
+        emit(AppResult.Failure(it))
+    }
+
+    override suspend fun deleteAllNoticeNotification() = flow {
+        emit(AppResult.Loading)
+
+        emit(AppResult.Success(
+            notificationDAO.deleteAllNoticeNotification()
+        ))
+    }.catch {
+        emit(AppResult.Failure(it))
+    }
+}
