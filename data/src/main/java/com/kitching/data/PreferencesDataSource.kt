2c0df27a27431df78c24dfbf73e87c19fabfc2e2
Eddie-Pak <foalstn@naver.com>
2025-08-20 17:32:11 +0900

    feat: PreferencesDataSource Inject 구현

--- data/src/main/java/com/kitching/data/PreferencesDataSource.kt
+++ data/src/main/java/com/kitching/data/PreferencesDataSource.kt
@@ -1,23 +1,22 @@
 package com.kitching.data
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+import javax.inject.Singleton
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "team")
-
-class PreferencesDataSource(private val context: Context) {
+@Singleton
+class PreferencesDataSource @Inject constructor(
+    private val preferencesData: DataStore<Preferences>
+) {
     companion object {
         private val TEAM_ID = stringPreferencesKey("team_id")
         private val USER_ID = stringPreferencesKey("user_id")
         private val FCM_TOKEN = stringPreferencesKey("fcm_token")
     }
-
-    private val preferencesData = context.dataStore
 
     suspend fun saveUserId(userId: String) = preferencesData.edit { preferences -> preferences[USER_ID] = userId }
 
@@ -28,7 +27,7 @@ class PreferencesDataSource(private val context: Context) {
     suspend fun getTeamId(): String = preferencesData.data.first()[TEAM_ID] ?: ""
 
     /** 토큰이 있는지 확인하고 있으면 업데이트, 없으면 새로 저장 */
-    suspend fun saveFcmToken(newToken: String) = context.dataStore.edit { preferences -> preferences[FCM_TOKEN] = newToken }
+    suspend fun saveFcmToken(newToken: String) = preferencesData.edit { preferences -> preferences[FCM_TOKEN] = newToken }
 
-    suspend fun getFcmToken(): String = context.dataStore.data.first()[FCM_TOKEN] ?: ""
+    suspend fun getFcmToken(): String = preferencesData.data.first()[FCM_TOKEN] ?: ""
 }
