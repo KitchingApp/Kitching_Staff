bacc419ead8ceef8b6092eb5090ff76a49e03e38
Eddie-Pak <foalstn@naver.com>
2025-08-21 00:33:48 +0900

    feat: RepositoryModule NotificationRepository update

--- data/src/main/java/com/kitching/data/di/RepositoryModule.kt
+++ data/src/main/java/com/kitching/data/di/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.kitching.data.di
 
 import com.kitching.data.repository.FcmTokenRepositoryImpl
 import com.kitching.data.repository.LoginRepositoryImpl
+import com.kitching.data.repository.NotificationRepositoryImpl
 import com.kitching.data.repository.OrderRepositoryImpl
 import com.kitching.data.repository.PrepRepositoryImpl
 import com.kitching.data.repository.RecipeRepositoryImpl
@@ -9,6 +10,7 @@ import com.kitching.data.repository.ScheduleRepositoryImpl
 import com.kitching.data.repository.TeamRepositoryImpl
 import com.kitching.domain.repository.FcmTokenRepository
 import com.kitching.domain.repository.LoginRepository
+import com.kitching.domain.repository.NotificationRepository
 import com.kitching.domain.repository.OrderRepository
 import com.kitching.domain.repository.PrepRepository
 import com.kitching.domain.repository.RecipeRepository
@@ -23,6 +25,12 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindNotificationRepository(
+        notificationRepositoryImpl: NotificationRepositoryImpl
+    ): NotificationRepository
 
     @Binds
     @Singleton
