a7b3a2723700a1fd0afd3acbf9a957efc31332dc
Eddie-Pak <foalstn@naver.com>
2025-08-20 16:58:14 +0900

    feat: Hilt Module 구현

---
+++ data/src/main/java/com/kitching/data/di/FirebaseModule.kt
@@ -0,0 +1,19 @@
+package com.kitching.data.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import jakarta.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+}
