44d81c3f584d980ce099b9ac6f2b660039959250
Eddie-Pak <foalstn@naver.com>
2025-08-21 14:44:33 +0900

    refactor: companion object 삭제

--- data/src/main/java/com/kitching/data/room/NotificationRoomDatabase.kt
+++ data/src/main/java/com/kitching/data/room/NotificationRoomDatabase.kt
@@ -1,8 +1,6 @@
 package com.kitching.data.room
 
-import android.content.Context
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.kitching.data.dao.NotificationDAO
 import com.kitching.data.entity.NoticeNotificationEntity
@@ -12,21 +10,4 @@ import com.kitching.data.entity.ScheduleNotificationEntity
 abstract class NotificationRoomDatabase : RoomDatabase() {
 
     abstract fun notificationDao(): NotificationDAO
-
-    companion object {
-        private lateinit var INSTANCE: NotificationRoomDatabase
-        internal fun getDatabase(context: Context): NotificationRoomDatabase {
-            if (!this::INSTANCE.isInitialized) {
-                synchronized(NotificationRoomDatabase::class.java) {
-                    INSTANCE =
-                        Room.databaseBuilder(
-                            context.applicationContext,
-                            NotificationRoomDatabase::class.java,
-                            "notification_database.db"
-                        ).build()
-                }
-            }
-            return INSTANCE
-        }
-    }
 }
